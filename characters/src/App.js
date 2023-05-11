70b0877db4d74beb1e1cf7b146ef165d562c7dd5
LauraCasta <laura.castanot@campusucc.edu.co>
2023-05-10 21:12:51 -0500

    Inicio de pagina characters

--- characters/src/App.js
+++ characters/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import log from './images/log.jpeg'
+import mar from './images/Marvel.jpeg'
+import Nav from './components/Nav/nav';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <header className="App-headerlogo">
+            <img src={log}  className="App-logo"/>
+            <div><Nav /></div>
+        </header>         
+           
+        <h1>Characters</h1>
       </header>
     </div>
+    </>
   );
 }
 
---
+++ characters/src/components/Nav/nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./nav.css"
+
+const nav = () => {
+    return (
+            <div className="nav">
+                <div className="nav_items">
+                    <a href="#"> Characters </a>
+                    <a href="#"> Comics </a>
+                    <a href="#"> Series </a>
+                    <a href="#"> Stories </a>                                        
+                </div>
+            </div>
+        )
+}
+
+export default nav
