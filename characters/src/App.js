c8816c44e0e23b32eab4eb3aa0b82ce876ff6268
LauraCasta <laura.castanot@campusucc.edu.co>
2023-05-15 15:53:57 -0500

    Crecion de menu de navegacion de comics

--- characters/src/App.js
+++ characters/src/App.js
@@ -1,19 +1,13 @@
 
 import './App.css';
-import log from './images/log.jpeg'
-import mar from './images/Marvel.jpeg'
 import Nav from './components/Nav/nav';
 
 function App() {
   return (
     <>
     <div className="App">
+      <div><Nav /></div>
       <header className="App-header">
-        <header className="App-headerlogo">
-            <img src={log}  className="App-logo"/>
-            <div><Nav /></div>
-        </header>         
-           
         <h1>Characters</h1>
       </header>
     </div>
