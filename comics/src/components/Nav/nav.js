bf8d685c67ec4100e60b55300497984698cf87ae
LauraCasta <laura.castanot@campusucc.edu.co>
2023-05-21 21:28:04 -0500

    url de navegacion

--- comics/src/components/Nav/nav.js
+++ comics/src/components/Nav/nav.js
@@ -7,10 +7,10 @@ const Nav = () => {
             <div className="nav">
                 <img src={log}  className="nav_logo"/>
                 <div className="nav_items">
-                    <a href="#" className="nav_items_cuadro"> CHARACTERS </a>
-                    <a href="#" className="nav_items_cuadro"> COMICS </a>
-                    <a href="#" className="nav_items_cuadro"> SERIES </a>
-                    <a href="#" className="nav_items_cuadro"> STORIES </a>                                        
+                    <a href="https://mcharacters6.s3.us-east-1.amazonaws.com/index.html" className="nav_items_cuadro"> CHARACTERS </a>
+                    <a href="https://comics6-bucket.s3.us-east-1.amazonaws.com/index.html" className="nav_items_cuadro"> COMICS </a>
+                    <a href="https://series6-bucket.s3.us-east-1.amazonaws.com/index.html" className="nav_items_cuadro"> SERIES </a>
+                    <a href="https://stories6-bucket.s3.us-east-1.amazonaws.com/index.html" className="nav_items_cuadro"> STORIES </a>                                        
                 </div>
                 
             </div>
