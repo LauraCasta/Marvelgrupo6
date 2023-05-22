1230b44463449d82bd959394c4ccdb9efc8d1cb1
juliantovar99 <julian.tovarr@campusucc.edu.co>
2023-05-21 21:32:35 -0500

    Se sube la información de series

---
+++ series/src/componentes/Nav/nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import log from '../images/log.jpeg'
+import "./nav.css"
+
+const nav = () => {
+    return (
+            <div className="nav">
+                <img src={log}  className="nav_logo"/>
+                <div className="nav_items">
+                    <a href="https://mcharacters6.s3.us-east-1.amazonaws.com/index.html" className="nav_items_cuadro"> CHARACTERS </a>
+                    <a href="http://comics6-bucket.s3-website-us-east-1.amazonaws.com" className="nav_items_cuadro"> COMICS </a>
+                    <a href="https://series6-bucket.s3.us-east-1.amazonaws.com/index.html" className="nav_items_cuadro"> SERIES </a>
+                    <a href="https://stories6-bucket.s3.us-east-1.amazonaws.com/index.html" className="nav_items_cuadro"> STORIES </a>
+                </div>
+            </div>
+        )
+}
+
+export default nav
